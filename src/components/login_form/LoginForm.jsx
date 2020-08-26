01b821b1f303f15e258a82ef76e879723187e31b
gottedev <gottedev@gmail.com>
2020-08-26 23:51:24 +0100

    corrects ui

--- src/components/login_form/LoginForm.jsx
+++ src/components/login_form/LoginForm.jsx
@@ -7,17 +7,23 @@ function LoginForm(props) {
   return (
     <div className="LoginForm-Container">
       <Card>
-        <Image src={logo} alt="green enery logo" />
+        <Image className="Logo-Container" src={logo} alt="green enery logo" />
         <div className="Title-Container">
           <h2>Example login screen</h2>
           <p>&nbsp;Getting started with Green.</p>
         </div>
-        <Input type="email" label="Email Address" />
-        <Input
-          type="checkbox"
-          label="Remember this device"
-          className="Checkbox-Container"
-        />
+        <div className="Field-Container">
+          <Input
+            type="email"
+            label="Email Address"
+            className="Email-Container"
+          />
+          <Input
+            type="checkbox"
+            label="Remember this device"
+            className="Remember-Container"
+          />
+        </div>
         <Button className="Login-Button" type="submit" text="Sign In" />
       </Card>
     </div>
