f79ec1f1ac7fcdd25b1168c10c593f69e46e1739
gottedev <gottedev@gmail.com>
2020-08-27 01:04:30 +0100

    adds interactivity and wraps form fields in form

--- src/components/login_form/LoginForm.jsx
+++ src/components/login_form/LoginForm.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Image, Input, Button } from "../assets";
 import logo from "../../icons/logo.svg";
 import "./loginform.scss";
 
 function LoginForm(props) {
+  const [email, setEmail] = useState("");
+  const [checked, setChecked] = useState(false);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const checkboxHandler = (e) => {
+    setChecked(e.target.checked);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert("Thanks for the Coding Test");
+  };
+
   return (
     <div className="LoginForm-Container">
       <Card>
@@ -12,19 +28,26 @@ function LoginForm(props) {
           <h2>Example login screen</h2>
           <p>&nbsp;Getting started with Green.</p>
         </div>
-        <div className="Field-Container">
-          <Input
-            type="email"
-            label="Email Address"
-            className="Email-Container"
-          />
-          <Input
-            type="checkbox"
-            label="Remember this device"
-            className="Remember-Container"
-          />
-        </div>
-        <Button className="Login-Button" type="submit" text="Sign In" />
+        <form onSubmit={submitHandler}>
+          <div className="Field-Container">
+            <Input
+              value={email}
+              type="email"
+              label="Email Address"
+              className="Email-Container"
+              onChange={emailHandler}
+              required
+            />
+            <Input
+              type="checkbox"
+              checked={checked}
+              label="Remember this device"
+              className="Remember-Container"
+              onChange={checkboxHandler}
+            />
+          </div>
+          <Button className="Login-Button" type="submit" text="Sign In" />
+        </form>
       </Card>
     </div>
   );
